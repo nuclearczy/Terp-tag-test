ebad5ed2b191473092aaaa36dfc8b86f69fe2417
ZuyangCao <nuclearczy@hotmail.com>
2019-11-30 15:36:48 -0500

    Try QR code detector without objectdetect namespace

--- src/localizer.cpp
+++ src/localizer.cpp
@@ -52,7 +52,7 @@ void Localizer::locateTag() {
   cv::Mat cvImageWithTag;
   cv_bridge::CvImagePtr cvPtr;
   //Seems version 3.4.2 or lower does not have QR code detector class, and ROS-kinetic has OpenCV 3.3.1
-  cv::objdetect::QRCodeDetector qrDetector;
+  cv::QRCodeDetector qrDetector;
   if(tagRecognition()){
     // Image encoding needs to be updated.
     cvPtr = cv_bridge::toCvCopy(camera, sensor_msgs::image_encodings::BGR8);
